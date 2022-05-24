11c5c340f189b031fefbb7e05a19279d58b54dd6
DanielShuneyko <donnidoll@gmail.com>
2022-05-24 23:26:23 +0500

    Create main.go

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+        "fmt"
+        "os"
+
+)
+
+
+// this is a comment
+
+func main() {
+    var adsa stringg
+    adsa = "Daniel"
+    fmt.Println("Hello, my name is", adsa)
+    os.Exit(1)
+}
